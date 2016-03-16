b65cc915a7f3403fecfb3c29dc77c344fe2b5e36
machakann <mckn@outlook.jp>
2016-03-16 20:46:12 +0800

    Update tests for dot command.

--- test/special-cases/souce.bat
+++ test/special-cases/souce.bat
@@ -2,7 +2,7 @@
 set VIM=vim
 if defined THEMIS_VIM set VIM=%THEMIS_VIM%
 
-%VIM% -u NONE -i NONE -N -n -e -s -S %1
+%VIM% -u NONE -i NONE -N -n -e -s -S %~dp0\test_dot.vim
 if %errorlevel% neq 0 goto ERROR
 echo Succeeded.
 exit /b 0
